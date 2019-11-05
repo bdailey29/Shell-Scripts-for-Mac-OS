a1a2c84730d965bbeeb0a5828fed2f2c8f39f149
bdailey29 <51727366+bdailey29@users.noreply.github.com>
2019-11-05 12:22:56 -0800

    Removed group/username specificity

--- Enable RemoteManagement and Login.sh
+++ Enable RemoteManagement and Login.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 ## Enable ARD on a machine.  Must be done after joining to the domain.
 
-# grant ssh rights to poplocaladmin 
-dscl . append /Groups/com.apple.access_ssh user poplocaladmin
+# grant ssh rights to local admin 
+dscl . append /Groups/com.apple.access_ssh user <username>
 systemsetup -setremotelogin on
 
-#Add POP\workstationadministrators to admin group.
-dseditgroup -o edit -a "POP\workstationadministrators" -t group admin
+#Add Domain\groupname to admin group.
+dseditgroup -o edit -a "Domian\groupname" -t group admin
 
-# Create local group that will contain pop\workstationadministrators
+# Create local group that will contain domain\groupname
 dscl . -create /Groups/ard_admin
 
 # The group must be manually assigned, and should be above 500.  We will standardize on 530
@@ -16,7 +16,7 @@ dscl . -create /Groups/ard_admin PrimaryGroupID "530"
 dscl . -create /Groups/ard_admin RealName "ard_admin"
 
 # Now the group is created, we must add the AD account to it
-dseditgroup -o edit -a "POP\workstationadministrators" -t group ard_admin
+dseditgroup -o edit -a "Domain\groupname" -t group ard_admin
 
 # Make sure access is only allowed for the Specified Users,
 /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -specifiedUsers
@@ -27,8 +27,8 @@ dseditgroup -o edit -a "POP\workstationadministrators" -t group ard_admin
 # Now that you have a local group with an AD group nested inside, you can give your group the necessary privileges via the ARD Kickstart command:
 /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -on -privs -all -users ard_admin -restart -agent
 
-#grant Remote Management privilges to poplocaladmin
-/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -users poplocaladmin,ard_admin,tysons,jamescook,robmacgregor,garretnobile,silviamdriz -access -on -privs -all
+#grant Remote Management privilges to local admin
+/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -users <username> -access -on -privs -all
 /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -activate -restart -console
 
 
